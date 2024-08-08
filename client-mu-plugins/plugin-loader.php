20598fddd397b54fcb88488c9f75fa7c71d65afa
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-08-08 05:29:06 +0300

    fix: load `my-inetgration` from `plugin-loader`

--- client-mu-plugins/plugin-loader.php
+++ client-mu-plugins/plugin-loader.php
@@ -9,15 +9,4 @@
  * @see https://docs.wpvip.com/technical-references/vip-codebase/client-mu-plugins-directory/
  */
 
-// phpcs:ignore Squiz.PHP.CommentedOutCode.Found
-// wpcom_vip_load_plugin( 'plugin-name' );
-
-/**
- * The above example requires the plugin to use a specific naming structure: /plugin-name/plugin-name.php
- * You can also specify a plugin's root file:
- * wpcom_vip_load_plugin( 'plugin-name/plugin.php' );
- *
- * wpcom_vip_load_plugin() only loads plugins from the `WP_PLUGIN_DIR` directory.
- * For client-mu-plugins use:
- * require __DIR__ . '/plugin-name/plugin-name.php';
- */
+wpcom_vip_load_plugin( 'my-integration/index.php' );
